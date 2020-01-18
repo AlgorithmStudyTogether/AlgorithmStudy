81eaff16137f8f1c9407de136b520c73e1f0a9f1
fangzheng <fangzheng@guanghe.tv>
2020-01-18 20:47:27 +0800

    feat: 🎸 添加代码

---
+++ Day15/HFZ/相同的树.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    var re = 0
+    if (re) {
+        return 0
+    }
+};
